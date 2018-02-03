4da32b31df8f330a6211537fad11b21292bed270
Zemian Deng <zemiandeng@gmail.com>
2018-02-03 15:00:23 -0500

    Add hello and learning modules

---
+++ learning/loops.go
@@ -0,0 +1,8 @@
+package learning/loops
+import "fmt"
+func main() {
+	fmt.Println("== loops")
+	for i := 0; i < 5; i++ {
+		fmt.Println("iteration# ${i}")
+	}
+}
---
+++ learning/main.go
@@ -0,0 +1,5 @@
+package main
+import "fmt"
+func main() {
+	fmt.Println("main func for learning module.")
+}
